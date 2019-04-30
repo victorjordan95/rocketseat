fcecbea18e650b89567e2659a8a246de74f536fd
Victor Jordan <victor.jordan@gauge.local>
2019-04-30 11:45:02 -0300

    adding typescript react course

---
+++ ReactTypescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Provider} from 'react-redux'
+import RepositoryList from './components/RepositoryList'
+import store from './store';
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <RepositoryList />
+    </Provider>
+  );
+}
+
+export default App;
